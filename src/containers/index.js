7bb56873507a6b331ebd5761c365fb3e6b104105
boris <taehyunkim3850@google.com>
2017-12-12 15:29:09 +0900

    WebPack 설정 수정
    react-hot-lodaer 3.0으로 업데이트 되면서 문제가 있었음

---
+++ src/containers/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Header } from '../components';
+
+class App extends React.Component {
+    render(){
+      return (
+            <Header/>
+      );
+    }
+}
+export { App };
