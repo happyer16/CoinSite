b0907a72895ef8527756214b8e1445fa8ed0b799
boris <taehyunkim3850>
2018-01-28 14:14:43 +0900

    ADD Coin 투자내역 테이블 기본 구축 ( Component : CoinList, Coin )

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,11 +1,73 @@
 import React from 'react';
-import { Header } from '../components';
 
 class App extends React.Component {
     render(){
       return (
-            <Header/>
+            <CoinList/>
       );
     }
 }
+
+class CoinList extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state={
+      coinData: [
+        {name : "이더리움", coinAmount : "200", buyAvg : "300000", buyAmount : "60000000", evaluationAmount : "1000000000"},
+        {name : "비트코인", coinAmount : "100", buyAvg : "1000000", buyAmount : "100000000", evaluationAmount : "1000000000"}
+      ]
+    }
+  }
+  render() {
+    return (
+      <div>
+        <h1> 투자내역 </h1>
+        <table>
+          <tr>
+            <th>보유코인</th>
+            <th>보유수량</th>
+            <th>매수평균가</th>
+            <th>매수금액</th>
+            <th>평가금액</th>
+          </tr>
+        {this.state.coinData.map((coin,i) => {
+          return (<Coin name = {coin.name}
+                        coinAmount = {coin.coinAmount}
+                        buyAvg = {coin.buyAvg}
+                        buyAmount ={coin.buyAmount}
+                        evaluationAmount={coin.evaluationAmount}
+                        key = {i}
+                  />);
+        })}
+        </table>
+      </div>
+
+    );
+  }
+}
+
+class Coin extends React.Component {
+  render() {
+    return (
+      <tr>
+        <td> {this.props.name} </td>
+        <td> {this.props.coinAmount} </td>
+        <td> {this.props.buyAvg} KRW </td>
+        <td> {this.props.buyAmount} KRW </td>
+        <td> {this.props.evaluationAmount} KRW</td>
+      </tr>
+    );
+  }
+}
+
+// Coin.propTypes = {
+//   name : PropTypes.string.isRequired, // 코인명
+//   coinAmount : PropTypes.number.isRequired // 보유수량
+// /**  buyAvg : PropTypes.number.isRequired, // 매수 평균가
+//   buyAmount : PropTypes.number.isRequired, // 매수 금액
+//   evaluationAmount : PropTypes.number.isRequired // 평가 금액
+//   **/
+// };
+
 export { App };
