a6692c8a23cb2d70eb7e4f67823bac0be8b0f331
boris <taehyunkim3850@google.com>
2018-02-09 12:55:23 +0900

    평가금액에 따른 CSS 스타일 적용 튜토리얼

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -1,14 +1,24 @@
 import React, { Component, PropTypes } from 'react';
 
 class Coin extends React.Component {
+
   render() {
+    var plusStyle = {
+        color: "#E06C61"
+    };
+
+    var minusStyle = {
+        color: "#56B6C2"
+    };
+
     return (
       <tr>
         <td> {this.props.name} </td>
         <td> {this.props.coinAmount} </td>
         <td> {Number(this.props.buyAvg).toLocaleString('en')} KRW </td>
         <td> {Number(this.props.buyAmount).toLocaleString('en')} KRW </td>
-        <td> {Number(this.props.evaluationAmount).toLocaleString('en')} KRW</td>
+        <td style={this.props.coinAmount>=100 ? plusStyle : minusStyle}> {Number(this.props.evaluationAmount).toLocaleString('en')} KRW</td>
+        {/** TODO 업비트 금액 따오는거 성공하면 실시간 수익율 계산 td 추가**/}
       </tr>
     );
   }
