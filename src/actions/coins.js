52a21b0b0ad5d182638f5f8f4d1295edccc5f3c3
boris <taehyunkim3850>
2018-03-07 23:27:15 +0900

    코인 투자 내역 추가 Dialog (4) - 코인 조회 해주는 Action 및 Reducer 생성

--- src/actions/coins.js
+++ src/actions/coins.js
@@ -1,7 +1,73 @@
 import axios from 'axios';
+import {
+  COIN_FETCH,
+  COIN_FETCH_SUCCESS,
+  COIN_FETCH_FAILURE,
+  COIN_REGISTER,
+  COIN_REGISTER_SUCCESS,
+  COIN_REGISTER_FAILURE
+} from './ActionTypes';
 
 export function infoRequest(coinname) {
   return (dispatch) => {
     return
   }
 }
+
+export function fetchCoins() {
+    return (dispatch) => {
+      // Inform fetchCoins API is starting
+      dispatch(fetch());
+      return axios.get('/api/coin/fetch')
+     .then((response) => {
+        // SUCCESE
+         dispatch(fetchSuccess(response));
+        // FIXME dispatch(fetchSuccess(response.data.coins));
+      }).catch((error) => {
+        // FAILED
+        dispatch(fetchFailure(error));
+      });
+    }
+}
+
+export const fetch = () => ({
+  type : COIN_FETCH
+});
+
+export const fetchSuccess = (coins) => ({
+  type : COIN_FETCH_SUCCESS,
+  coins
+});
+
+export const fetchFailure = () => ({
+  type : COIN_FETCH_FAILURE
+});
+
+/* ADD COIN */
+export function registerRequest(coinname, amount) {
+  return (dispatch) => {
+    // Inform addcoin API is starting
+    dispatch(register());
+
+    return axios.post('/api/coin/register', { coinname, amount })
+    .then((response) => {
+      // SUCCESE
+      dispatch(registerSuccess());
+    }).catch((error) => {
+      // FAILED
+      dispatch(registerFailure(error.response.data.code));
+    });
+  }
+}
+
+export const register = () => ({
+  type : COIN_REGISTER
+});
+
+export const registerSuccess = () => ({
+  type : COIN_REGISTER_SUCCESS
+});
+
+export const registerFailure = () => ({
+  type : COIN_REGISTER_FAILURE
+});
--- src/reducers/coins.js
+++ src/reducers/coins.js
@@ -6,6 +6,10 @@ import update from 'react-addons-update';
 //   name: 'eth'
 // };
 
+const coinsInitialState = {
+    coins: []
+};
+
 const coin = (state,action) => {
   switch(action.type) {
     case types.ADD_COIN:
@@ -17,13 +21,26 @@ const coin = (state,action) => {
       return state;
   }
 }
-function coins(state = [], action){
+
+function coins(state = coinsInitialState, action){
+  console.log(action);
   switch (action.type) {
     case types.ADD_COIN :
       return [
         ...state, coin(undefined,action)
       ];
+    case types.COIN_FETCH_SUCCESS :
+      return update(state, {
+        coins: action.coins
+      });
 
+    case types.COIN_REGISTER :
+      return update(state, {
+        register: {
+          status: { $set: 'WAITING' },
+          error : { $set: -1 }
+        }
+      });
     default :
       return state;
   }
